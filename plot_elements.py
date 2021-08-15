9bc55debc712448b857add6db1da42e6c8fb72cf
Gustavol Enrique Olivos Ramirez <tavolivos@gmail.com>
2021-08-15 01:16:27 -0500

    Update plot_elements.py

--- plot_elements.py
+++ plot_elements.py
@@ -12,20 +12,26 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('data_metales.csv').replace("SiO2",'SiO$_2$')
 
-fig,ax = plt.subplots(figsize=(15,5))
+fig,ax = plt.subplots(figsize=(20,6))
 
 labels = ['0','0','0.01', '1', '100','10000']
 
 elemento = df['elemento']
 concentracion = df['concentracion']
 
-#sns.set(style="white")
-g = sns.barplot(elemento, concentracion, alpha=1, data=df,
+isns.set_context(mode='paper', fontfamily='sans-serif', fontweight='bold', rc=None)
+sns.set(style="dark", font_scale=2)
+
+rc('font', weight='bold')
+
+g = sns.barplot(elemento, concentracion, alpha=1, data=df, 
             order=df.sort_values('concentracion',ascending = False).elemento)
 g.set_yscale("log")
 #g.axhline(1, color='red')
-ax.set_ylabel('Quantity (ppm)', fontsize=14)
-ax.set_xlabel('Element', fontsize=14)
+ax.set_ylabel('Quantity (ppm)', fontsize=22)
+ax.set_xlabel('Element', fontsize=22)
+plt.xticks(fontsize=16)
+plt.yticks(fontsize=12)
 plt.ylim(0,np.exp(10))
 
 #ax.set_xticklabels(labels)
@@ -33,10 +39,10 @@ plt.xlim(-1,32)
 for bar in g.patches:
     g.annotate(format(bar.get_height(), '.2f'),
                (bar.get_x() + bar.get_width() / 2,
-                bar.get_height()+ 2.4*bar.get_height()), ha='center', va='center',
-               size=12, xytext=(0, 8), textcoords='offset points',
+                bar.get_height()+ 6.1*bar.get_height()), ha='center', va='center',
+               size=16, xytext=(0, 8), textcoords='offset points',
               rotation=90)
 sns.despine()
+fig.tight_layout()
 fig.savefig("elements.png", format='png', dpi=800, transparent=True)
 plt.show()
-
